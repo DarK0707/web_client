b75710f31cd3fef206d2a7333843a87cfe88c7fa
DarK0707 <sezerpro7835@gmail.com>
2022-02-01 21:45:47 +0300

    dsa

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -10,7 +10,7 @@ const Footer = () => {
             <div className="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
               <h6 className="text-uppercase fw-bold mb-4">
                 <i className="fas fa-gem me-3" />
-                ShineSafrly
+                ShineSafely
               </h6>
               <p>
                 Kokteyl ve kahve şurupları, meyve püresi ile başladığımız üretim
