7f7e97c9739f3dafa9e7aa4c8d46aa20725d17a6
hasanaliarikannn <hasanaliarikan07@hotmail.com>
2022-02-01 20:55:50 +0300

    fix api

--- src/actions/userActions.js
+++ src/actions/userActions.js
@@ -41,7 +41,7 @@ export const login = (email, password) => async (dispatch) => {
     };
 
     const { data } = await axios.post(
-      "/api/users/login",
+      "https://shinesafely.vercel.app/api/users/login",
       { email, password },
       config
     );
@@ -85,7 +85,7 @@ export const register = (name, email, password) => async (dispatch) => {
     };
 
     const { data } = await axios.post(
-      "/api/users",
+      "https://shinesafely.vercel.app/api/users",
       { name, email, password },
       config
     );
@@ -130,7 +130,7 @@ export const getUserDetails = (id) => async (dispatch, getState) => {
       },
     };
 
-    const { data } = await axios.get(`/api/users/${id}`, config);
+    const { data } = await axios.get(`https://shinesafely.vercel.app/api/users/${id}`, config);
 
     dispatch({
       type: USER_DETAILS_SUCCESS,
@@ -164,7 +164,7 @@ export const updateUserProfile = (user) => async (dispatch, getState) => {
       },
     };
 
-    const { data } = await axios.put(`/api/users/profile`, user, config);
+    const { data } = await axios.put(`https://shinesafely.vercel.app/api/users/profile`, user, config);
 
     dispatch({
       type: USER_UPDATE_PROFILE_SUCCESS,
@@ -204,7 +204,7 @@ export const listUsers = () => async (dispatch, getState) => {
       },
     };
 
-    const { data } = await axios.get(`/api/users`, config);
+    const { data } = await axios.get(`https://shinesafely.vercel.app/api/users`, config);
 
     dispatch({
       type: USER_LIST_SUCCESS,
